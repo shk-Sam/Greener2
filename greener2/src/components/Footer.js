3aee00384cf3e1abd79f03306a9b585654167001
shk-Sam <shaikhsaad78890@gmail.com>
2024-12-18 20:10:28 +0530

    greener

---
+++ greener2/src/components/Footer.js
@@ -0,0 +1,40 @@
+function Footer (){
+    return(
+        <footer>
+            <div class="imgInfo">
+                <img src="../Website 3/img/logo.png" alt=""/>
+                <p>Lorem ispum dolor sit amet,<br/>consectetur adipiscing elit.Nam nec<br/>eroc ut nunc luctus consecteturvel ac<br/>justo.</p>
+                {/* <svg xmlns="http://www.w3.org/2000/svg" width="64" height="64" shape-rendering="geometricPrecision" text-rendering="geometricPrecision" image-rendering="optimizeQuality" fill-rule="evenodd" clip-rule="evenodd" viewBox="0 0 640 640"><path d="M640.012 121.513c-23.528 10.524-48.875 17.516-75.343 20.634 27.118-16.24 47.858-41.977 57.756-72.615-25.347 14.988-53.516 25.985-83.363 31.866-24-25.5-58.087-41.35-95.848-41.35-72.508 0-131.21 58.736-131.21 131.198 0 10.228 1.134 20.232 3.355 29.882-109.1-5.528-205.821-57.757-270.57-137.222a131.423 131.423 0 0 0-17.764 66c0 45.497 23.102 85.738 58.347 109.207-21.508-.638-41.74-6.638-59.505-16.359v1.642c0 63.627 45.225 116.718 105.32 128.718-11.008 2.988-22.63 4.642-34.606 4.642-8.48 0-16.654-.874-24.78-2.35 16.783 52.11 65.233 90.095 122.612 91.205-44.989 35.245-101.493 56.233-163.09 56.233-10.63 0-20.988-.65-31.334-1.89 58.229 37.359 127.206 58.997 201.31 58.997 241.42 0 373.552-200.069 373.552-373.54 0-5.764-.13-11.35-.366-16.996 25.642-18.343 47.87-41.493 65.469-67.844l.059-.059z"/></svg> */}
+                {/* <svg xmlns="http://www.w3.org/2000/svg" shape-rendering="geometricPrecision" text-rendering="geometricPrecision" image-rendering="optimizeQuality" fill-rule="evenodd" clip-rule="evenodd" viewBox="0 0 509 507.14"><path fill-rule="nonzero" d="M509 254.5C509 113.94 395.06 0 254.5 0S0 113.94 0 254.5C0 373.86 82.17 474 193.02 501.51V332.27h-52.48V254.5h52.48v-33.51c0-86.63 39.2-126.78 124.24-126.78 16.13 0 43.95 3.17 55.33 6.33v70.5c-6.01-.63-16.44-.95-29.4-.95-41.73 0-57.86 15.81-57.86 56.91v27.5h83.13l-14.28 77.77h-68.85v174.87C411.35 491.92 509 384.62 509 254.5z"/></svg> */}
+            </div>
+            <div class="Quicks">
+                <h5>Quicks Links</h5>
+                <a href="">Home</a>
+                <a href="">plant</a>
+                <a href="">Plant Collection</a>
+                <a href="">About</a>
+                <a href="">Blog</a>
+                <a href="">Conatct</a>
+            </div>
+            <div class="About">
+                <h5>About Us</h5>
+                <a href="">Our Company</a>
+                <a href="">Privacy Policy</a>
+                <a href="">Product</a>
+                <a href="">How to Buy</a>
+            </div>
+            <div class="Contact">
+                <h5>Contact</h5>
+                <span>
+                    {/* <?xml version="1.0" encoding="utf-8"?><svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" width="118.875px" height="122.877px" viewBox="0 0 118.875 122.877" enable-background="new 0 0 118.875 122.877" xml:space="preserve"><g><path fill-rule="evenodd" clip-rule="evenodd" d="M1.432,20.714c9.427,6.476,18.779,12.989,26.673,21.375 C23.58,62.076,41.64,78.045,58.111,87.453c6.448,3.683,9.298,6.265,16.476,5.024l28.268,27.696 C49.657,138.039-10.045,63.084,1.432,20.714L1.432,20.714z M81.874,85.356l6.201-6.298c1.788-1.819,4.74-1.842,6.559-0.053 l22.862,22.509c1.818,1.789,1.841,4.741,0.053,6.559l-6.203,6.299c-1.789,1.818-4.741,1.841-6.56,0.052L81.924,91.915 C80.105,90.127,80.083,87.175,81.874,85.356L81.874,85.356z M7.004,7.467l4.5-5.464c1.929-2.342,5.425-2.68,7.767-0.751 l23.405,19.281c2.342,1.931,2.677,5.425,0.75,7.77l-4.502,5.461c-1.93,2.345-5.425,2.68-7.768,0.751L7.755,15.234 C5.413,13.305,5.073,9.809,7.004,7.467L7.004,7.467z"/></g></svg> */}
+                    +1121154454</span>
+                    <span>
+                        {/* <?xml version="1.0" encoding="utf-8"?><svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" width="122.88px" height="78.607px" viewBox="0 0 122.88 78.607" enable-background="new 0 0 122.88 78.607" xml:space="preserve"><g><path fill-rule="evenodd" clip-rule="evenodd" d="M61.058,65.992l24.224-24.221l36.837,36.836H73.673h-25.23H0l36.836-36.836 L61.058,65.992L61.058,65.992z M1.401,0l59.656,59.654L120.714,0H1.401L1.401,0z M0,69.673l31.625-31.628L0,6.42V69.673L0,69.673z M122.88,72.698L88.227,38.045L122.88,3.393V72.698L122.88,72.698z"/></g></svg> */}
+                        lampunggardening@garden.com
+                    </span>
+                <address>Jendral sudirman 111, Pahoman<br/>Bandar Lampung, Lampung</address>
+            </div>
+        </footer>
+    )
+}
+export default Footer
---
+++ greener2/src/components/Home.js
@@ -0,0 +1,181 @@
+function Home (){
+    return(
+        <>
+          <div class="container">
+        <div class="heading">
+            <h1>make your space<br/>
+                <div>
+                    <span>Greener</span>
+                    <img src="" alt=""/> 
+                </div>
+                with 
+                <span>plants</span>
+            </h1>
+            <p>Lorem ispum dolor sit amet, consectetur adipiscing elit .In<br/>orci ligula,</p>
+            <div class="buttons">
+                <a href="#" class="firstChild">Shop Plant
+                    {/* <svg xmlns="http://www.w3.org/2000/svg" shape-rendering="geometricPrecision" text-rendering="geometricPrecision" image-rendering="optimizeQuality" fill-rule="evenodd" clip-rule="evenodd" viewBox="0 0 512 376.83"><path fill-rule="nonzero" d="M355.12 372.7a12.026 12.026 0 0 1-17.09 1.06c-5-4.47-5.46-12.2-1.04-17.25l136.05-155.82H12.15c-6.71 0-12.15-5.5-12.15-12.28 0-6.77 5.44-12.27 12.15-12.27h460.9L336.99 20.32c-4.42-5.05-3.96-12.78 1.04-17.25 5.01-4.47 12.66-4 17.09 1.05l153.67 176c4.17 4.55 4.33 11.64.17 16.39L355.12 372.7z"/></svg> */}
+                </a>
+                <a href="#" class="secondChild">About us
+                    {/* <?xml version="1.0" encoding="utf-8"?><svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" width="122.881px" height="9.737px" viewBox="0 0 122.881 9.737" enable-background="new 0 0 122.881 9.737" xml:space="preserve"><g><path d="M117.922,0.006C117.951,0.002,117.982,0,118.012,0c0.656,0,1.285,0.132,1.861,0.371c0.014,0.005,0.025,0.011,0.037,0.017 c0.584,0.248,1.107,0.603,1.543,1.039c0.881,0.88,1.428,2.098,1.428,3.441c0,0.654-0.133,1.283-0.371,1.859 c-0.248,0.6-0.609,1.137-1.057,1.583c-0.445,0.445-0.98,0.806-1.58,1.055v0.001c-0.576,0.238-1.205,0.37-1.861,0.37 c-0.029,0-0.061-0.002-0.09-0.006c-37.654,0-75.309,0.001-112.964,0.001c-0.029,0.004-0.059,0.006-0.09,0.006 c-0.654,0-1.283-0.132-1.859-0.371c-0.6-0.248-1.137-0.609-1.583-1.056C0.981,7.865,0.621,7.33,0.372,6.73H0.371 C0.132,6.154,0,5.525,0,4.869C0,4.215,0.132,3.586,0.371,3.01c0.249-0.6,0.61-1.137,1.056-1.583 c0.881-0.881,2.098-1.426,3.442-1.426c0.031,0,0.061,0.002,0.09,0.006C42.613,0.006,80.268,0.006,117.922,0.006L117.922,0.006z"/></g></svg> */}
+                </a>
+            </div>
+        </div>
+        <div class="rightBoxes">
+            <div class="gridBox">
+                <div sec1>
+                    <h2>Section 1</h2>
+                    <p>Lorem ispum dolor sit amet, consectetur adipiscing<br/>elit .In orci ligula. consectetur sit amet</p>
+                    <img src="" alt=""/>
+                </div>
+                <div class="sec2"> 
+                    <h2>Section 2</h2>
+                    <p>Lorem ispum dolor sit amet.</p>
+                    <img src="" alt=""/>
+                </div>
+                <div class="sec3"> 
+                    <h2>Section 3</h2>
+                    <p>Lorem ispum dolor sit amet.</p>
+                    <img src="" alt=""/>
+                </div>
+            </div>
+        </div>
+    </div>
+        <section class="greenSec">
+            <div  class="wrapper">
+                <h2>BEST SELLING<br/>Plant Collection</h2>
+                <p>Check out our collection, we got every plant you need here!<br/>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; Let's grow it together </p>
+                <div class="container">
+                    <div class="plantCard">
+                        <div class="thumbnail">
+                            <img src="../Website 3/img/interior-home-plant-in-pot-png.webp" alt=""/>
+                        </div>
+                        <div class="info">
+                            <h3>Plant 1</h3>
+                            <p>Lorem Ipsum Dolor<br/> Sit Amet.</p>
+                            <span class="price">$20,00</span>
+                            <a href="" class="button">Buy now</a>
+                        </div>
+                    </div>
+                    <div class="plantCard">
+                        <div class="thumbnail">
+                            <img src="../Website 3/img/interior-home-plant-in-pot-png.webp" alt=""/>
+                        </div>
+                        <div class="info">
+                            <h3>Plant 1</h3>
+                            <p>Lorem Ipsum Dolor<br/> Sit Amet.</p>
+                            <span class="price">$20,00</span>
+                            <a href="" class="button">Buy now</a>
+                        </div>
+                    </div>
+                    <div class="plantCard">
+                        <div class="thumbnail">
+                            <img src="../Website 3/img/interior-home-plant-in-pot-png.webp" alt=""/>
+                        </div>
+                        <div class="info">
+                            <h3>Plant 1</h3>
+                            <p>Lorem Ipsum Dolor<br/> Sit Amet.</p>
+                            <span class="price">$20,00</span>
+                            <a href="" class="button">Buy now</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <section class="imgPara">
+            <div class="img">
+                <img src="../Website 3/img/pngtree-indoor-plant-png-image_6129530.png" alt="tree"/>
+            </div>
+            <div class="info">
+                <div class="about">
+                    <h2>What's Best From<br/>Our 
+                        <span>Plants</span>
+                    </h2>
+                    <p>
+                        Lorem ipsum dolor sit amet consectetur, adipisicing elit.<br/>Quam cum repudiandae sed similique ipsam<br/>doloremque iste praesentium est debitis<br/>sapientesit atque,libero explicabo impedit<br/>quibusdam molestiae commodi ut obcaecati<br/>totam sequi earum non ipsa.
+                    </p>
+                </div>
+                <div class="buttons">
+                    <a href="" class="firstChild">Shop Plant
+                        {/* <svg xmlns="http://www.w3.org/2000/svg" shape-rendering="geometricPrecision" text-rendering="geometricPrecision" image-rendering="optimizeQuality" fill-rule="evenodd" clip-rule="evenodd" viewBox="0 0 512 376.83"><path fill-rule="nonzero" d="M355.12 372.7a12.026 12.026 0 0 1-17.09 1.06c-5-4.47-5.46-12.2-1.04-17.25l136.05-155.82H12.15c-6.71 0-12.15-5.5-12.15-12.28 0-6.77 5.44-12.27 12.15-12.27h460.9L336.99 20.32c-4.42-5.05-3.96-12.78 1.04-17.25 5.01-4.47 12.66-4 17.09 1.05l153.67 176c4.17 4.55 4.33 11.64.17 16.39L355.12 372.7z"/></svg> */}
+                    </a>
+                    <a href="" class="secondChild">Read More
+                        {/* <?xml version="1.0" encoding="utf-8"?><svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" width="122.881px" height="9.737px" viewBox="0 0 122.881 9.737" enable-background="new 0 0 122.881 9.737" xml:space="preserve"><g><path d="M117.922,0.006C117.951,0.002,117.982,0,118.012,0c0.656,0,1.285,0.132,1.861,0.371c0.014,0.005,0.025,0.011,0.037,0.017 c0.584,0.248,1.107,0.603,1.543,1.039c0.881,0.88,1.428,2.098,1.428,3.441c0,0.654-0.133,1.283-0.371,1.859 c-0.248,0.6-0.609,1.137-1.057,1.583c-0.445,0.445-0.98,0.806-1.58,1.055v0.001c-0.576,0.238-1.205,0.37-1.861,0.37 c-0.029,0-0.061-0.002-0.09-0.006c-37.654,0-75.309,0.001-112.964,0.001c-0.029,0.004-0.059,0.006-0.09,0.006 c-0.654,0-1.283-0.132-1.859-0.371c-0.6-0.248-1.137-0.609-1.583-1.056C0.981,7.865,0.621,7.33,0.372,6.73H0.371 C0.132,6.154,0,5.525,0,4.869C0,4.215,0.132,3.586,0.371,3.01c0.249-0.6,0.61-1.137,1.056-1.583 c0.881-0.881,2.098-1.426,3.442-1.426c0.031,0,0.061,0.002,0.09,0.006C42.613,0.006,80.268,0.006,117.922,0.006L117.922,0.006z"/></g></svg> */}
+                    </a>
+                </div>
+            </div>
+        </section>
+        <section class="fourthSec">
+            <div class="proverb">
+                <h2>start gardening<br/>and grow your<br/>own plant!</h2>
+                <img src="../Website 3/img/sand pots.jpg" alt=""/>
+                <div class="paraButton">
+                    <p>Lorem ispum dolor sit amet,<br/>consectetur adipiscing elit</p>
+                    <a href=""> Read More
+                        {/* <?xml version="1.0" encoding="utf-8"?><svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" width="122.881px" height="9.737px" viewBox="0 0 122.881 9.737" enable-background="new 0 0 122.881 9.737" xml:space="preserve"><g><path d="M117.922,0.006C117.951,0.002,117.982,0,118.012,0c0.656,0,1.285,0.132,1.861,0.371c0.014,0.005,0.025,0.011,0.037,0.017 c0.584,0.248,1.107,0.603,1.543,1.039c0.881,0.88,1.428,2.098,1.428,3.441c0,0.654-0.133,1.283-0.371,1.859 c-0.248,0.6-0.609,1.137-1.057,1.583c-0.445,0.445-0.98,0.806-1.58,1.055v0.001c-0.576,0.238-1.205,0.37-1.861,0.37 c-0.029,0-0.061-0.002-0.09-0.006c-37.654,0-75.309,0.001-112.964,0.001c-0.029,0.004-0.059,0.006-0.09,0.006 c-0.654,0-1.283-0.132-1.859-0.371c-0.6-0.248-1.137-0.609-1.583-1.056C0.981,7.865,0.621,7.33,0.372,6.73H0.371 C0.132,6.154,0,5.525,0,4.869C0,4.215,0.132,3.586,0.371,3.01c0.249-0.6,0.61-1.137,1.056-1.583 c0.881-0.881,2.098-1.426,3.442-1.426c0.031,0,0.061,0.002,0.09,0.006C42.613,0.006,80.268,0.006,117.922,0.006L117.922,0.006z"/></g></svg> */}
+                    </a>
+                </div>
+            </div>
+        </section>
+        <section class="fifthSec">
+            <div class="aboutHouseplnt">
+                <h2>know more about<br/>houseplant</h2>
+                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit.<br/>Placeat numquam temporibus quaerat ipsum doloribus<br/>facere deserunt,autem consequatur ullam ipsam.<br/>Donate vulputateorci utmi sodales finibes</p>
+                <div class="buttons">
+                    <a href="" class="firstChild">Our Blog
+                        {/* <svg xmlns="http://www.w3.org/2000/svg" shape-rendering="geometricPrecision" text-rendering="geometricPrecision" image-rendering="optimizeQuality" fill-rule="evenodd" clip-rule="evenodd" viewBox="0 0 512 376.83"><path fill-rule="nonzero" d="M355.12 372.7a12.026 12.026 0 0 1-17.09 1.06c-5-4.47-5.46-12.2-1.04-17.25l136.05-155.82H12.15c-6.71 0-12.15-5.5-12.15-12.28 0-6.77 5.44-12.27 12.15-12.27h460.9L336.99 20.32c-4.42-5.05-3.96-12.78 1.04-17.25 5.01-4.47 12.66-4 17.09 1.05l153.67 176c4.17 4.55 4.33 11.64.17 16.39L355.12 372.7z"/></svg> */}
+                    </a>
+                    <a href="" class="secondChild">Read More
+                        {/* <?xml version="1.0" encoding="utf-8"?><svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" width="122.881px" height="9.737px" viewBox="0 0 122.881 9.737" enable-background="new 0 0 122.881 9.737" xml:space="preserve"><g><path d="M117.922,0.006C117.951,0.002,117.982,0,118.012,0c0.656,0,1.285,0.132,1.861,0.371c0.014,0.005,0.025,0.011,0.037,0.017 c0.584,0.248,1.107,0.603,1.543,1.039c0.881,0.88,1.428,2.098,1.428,3.441c0,0.654-0.133,1.283-0.371,1.859 c-0.248,0.6-0.609,1.137-1.057,1.583c-0.445,0.445-0.98,0.806-1.58,1.055v0.001c-0.576,0.238-1.205,0.37-1.861,0.37 c-0.029,0-0.061-0.002-0.09-0.006c-37.654,0-75.309,0.001-112.964,0.001c-0.029,0.004-0.059,0.006-0.09,0.006 c-0.654,0-1.283-0.132-1.859-0.371c-0.6-0.248-1.137-0.609-1.583-1.056C0.981,7.865,0.621,7.33,0.372,6.73H0.371 C0.132,6.154,0,5.525,0,4.869C0,4.215,0.132,3.586,0.371,3.01c0.249-0.6,0.61-1.137,1.056-1.583 c0.881-0.881,2.098-1.426,3.442-1.426c0.031,0,0.061,0.002,0.09,0.006C42.613,0.006,80.268,0.006,117.922,0.006L117.922,0.006z"/></g></svg> */}
+                    </a>
+                </div>
+            </div>
+            <div class="img">
+                <img src="../Website 3/img/pngtree-indoor-plant-png-image_6129530.png" alt=""/>
+            </div>
+        </section>
+        <section class="greenSec2">
+            <div  class="wrapper">
+                <h2> what they say<br/>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;reviews</h2>
+                <p>Check out our collection, we got every plant you need here!<br/>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; Let's grow it together </p>
+                <div class="container">
+                    <div class="plantCard">
+                        <div class="thumbnail">
+                            <img src="../Website 3/img/bradley.jpg" alt=""/>
+                            <h3>Bradley
+                            </h3>
+                            <div class="bttm"></div>
+                        </div>
+                        <div class="info">
+                            <p>Lorem ispum dolor sit amet,<br/>consectetur adipiscing elit.<br/>Nam nem eroc ut nunc luctus<br/>consecteturvel ac justo</p>
+                        </div>
+                    </div>
+                    <div class="plantCard">
+                        <div class="thumbnail">
+                            <img src="../Website 3/img/SAVANNAH.jpg" alt=""/>
+                            <h3>Savannah    
+                            </h3> 
+                            <div class="bttm"></div>
+                        </div>
+                        <div class="info">
+                            <p>Lorem ispum dolor sit amet,<br/>consectetur adipiscing elit.<br/>Nam nem eroc ut nunc luctus<br/>consecteturvel ac justo</p>
+                        </div>
+                    </div>
+                    <div class="plantCard">
+                        <div class="thumbnail">
+                            <img src="../Website 3/img/sarah.jpg" alt=""/>
+                            <h3>Sarah
+                            </h3>
+                            <div class="bttm"></div>
+                        </div>
+                        <div class="info">
+                            <p>Lorem ispum dolor sit amet,<br/>consectetur adipiscing elit.<br/>Nam nem eroc ut nunc luctus<br/>consecteturvel ac justo</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        </>
+    )
+}
+export default Home
